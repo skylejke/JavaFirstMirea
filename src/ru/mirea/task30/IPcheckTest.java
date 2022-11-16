d680a781cccc5254a2ed28e53a7dd46a7573338a
Danil Damarad <danilmega2003@gmail.com>
2022-11-16 19:09:39 +0300

    Практическая работа №30

--- src/ru/mirea/task30/IPcheckTest.java
+++ src/ru/mirea/task30/IPcheckTest.java
@@ -1,17 +1,16 @@
 package ru.mirea.task30;
 
-import org.junit.Assert;
-import org.junit.Test;
 
+import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class IPcheckTest {
 
     @Test
     public void checkIPTest() {
-        Assert.assertEquals(true, IPcheck.checkIP("127.0.0.1"));
-        Assert.assertEquals(true, IPcheck.checkIP("255.255.255.0"));
-        Assert.assertEquals(false, IPcheck.checkIP("1300.6.7.8"));
-        Assert.assertEquals(false, IPcheck.checkIP("abc.def.gha.bcd"));
+        assertTrue(IPcheck.checkIP("127.0.0.1"));
+        assertTrue(IPcheck.checkIP("255.255.255.0"));
+        assertFalse(IPcheck.checkIP("1300.6.7.8"));
+        assertFalse(IPcheck.checkIP("abc.def.gha.bcd"));
     }
 }
